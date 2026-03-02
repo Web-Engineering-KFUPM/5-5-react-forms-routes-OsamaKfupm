4b5ca53afbd1ac1d58c22e82a0af90a9dc2fa6b6
OsamaKfupm <s202242980@kfupm.edu.sa>
2026-03-02 10:34:22 +0300

    1st sub

--- 5-5-react-route-form/src/pages/Registration.jsx
+++ 5-5-react-route-form/src/pages/Registration.jsx
@@ -4,12 +4,28 @@ export default function Registration() {
   const [email, setEmail] = useState("");
 
   const [errors, setErrors] = useState({});
-
+  const [password, setPassword] = useState("");
+  const [gender, setGender] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     {/*Form validations*/}
+     const nextErrors = {};
 
-    // alert(`Regiteration submit: ${email}`);
+     // Email validation
+     if (!email.trim()) nextErrors.email = "Email is required";
+     else if (!(email.includes("@") && email.endsWith(".com")))
+       nextErrors.email = "Enter a valid email address";
+
+     // Password validation
+     if (!password.trim()) nextErrors.password = "Password is required";
+
+     // Gender validation
+     if (!gender) nextErrors.gender = "Please select your gender";
+
+     setErrors(nextErrors);
+     if (Object.keys(nextErrors).length > 0) return; // stop form submit if errors
+
+    alert(`Regiteration submit: ${email}`);
   };
 
   return (
@@ -34,17 +50,58 @@ export default function Registration() {
           {errors.email && (
             <p id="email-error" className="error">{errors.email}</p>
           )}
+
+          
+
         </div>
         <div className="form-row">
            {/*password*/}
+           <label htmlFor="password">Password</label>
+             <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+             />
+
+             {errors.password && (
+              <p id="password-error" className="error">
+                {errors.password}
+              </p>
+              )}
         </div>
 
         <fieldset className="form-row">
           {/*Radio Button for gender*/}
+            <legend>Gender</legend>
+            <label className="radio">
+              <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={gender === "male"}
+              onChange={(e) => setGender(e.target.value)}
+              /> Male
+             </label>
+
+               <label className="radio">
+                  <input
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    checked={gender === "female"}
+                    onChange={(e) => setGender(e.target.value)}
+                  />
+                  Female
+                </label>
+
+              {errors.gender && (
+                <p className="error">{errors.gender}</p>
+              )}
         </fieldset>
 
           {/*Disable the submit button until all requirements met*/}
-        <button type="submit" className="btn">Register</button>
+        <button type="submit" disabled={!email || !password || !gender}>Register</button>
       </form>
 
       <div className="card info">
